54bc1fd7f5dd70e1dc5cad3f69b7aeffe578de64
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2026-01-07 16:36:01 +0600

    up

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,34 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/api/actions/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST,PUT,OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "Content-Type, Authorization, Content-Encoding, Accept-Encoding, X-Action-Version, X-Blockchain-Ids",
+          },
+          {
+            key: "Access-Control-Expose-Headers",
+            value: "X-Action-Version, X-Blockchain-Ids",
+          },
+          { key: "X-Action-Version", value: "1" },
+          {
+            key: "X-Blockchain-Ids",
+            value: "solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp",
+          },
+        ],
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
