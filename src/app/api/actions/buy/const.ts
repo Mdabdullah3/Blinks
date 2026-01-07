b439b332f33ab9413f80f23cab59ec0984fd35d0
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2026-01-07 16:23:37 +0600

    up

--- src/app/api/actions/buy/const.ts
+++ src/app/api/actions/buy/const.ts
@@ -1,11 +1,10 @@
-// app/api/actions/buy/const.ts
 export const ACTIONS_CORS_HEADERS: Record<string, string> = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET,POST,PUT,OPTIONS",
   "Access-Control-Allow-Headers":
-    "Content-Type, Authorization, Content-Encoding, Accept-Encoding, X-Accept-Action-Version, X-Accept-Blockchain-Ids",
+    "Content-Type, Authorization, Content-Encoding, Accept-Encoding, X-Action-Version, X-Blockchain-Ids", // Remove 'Accept-' prefix here
   "Access-Control-Expose-Headers": "X-Action-Version, X-Blockchain-Ids",
   "Content-Type": "application/json",
-  "X-Action-Version": "2.1.3",
+  "X-Action-Version": "1", // Change from 2.1.3 to 1
   "X-Blockchain-Ids": "solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp",
 };
